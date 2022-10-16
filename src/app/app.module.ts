8dc0264ba8da9db7d14cdcd40f77777a8595655e
HelloImRosie <rosie.liurx@gmail.com>
2022-10-15 18:06:46 -0700

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,8 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
